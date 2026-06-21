d1bf7542db1e77a623491b2a441487924d15990f
JaderoChan <c_dl_cn@outlook.com>
2026-06-21 21:20:48 +0800

    Update: Add K-Means algo

---
+++ k-means/example/dump_image_theme_color/main.cpp
@@ -0,0 +1,87 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+#include <kmeans.hpp>
+#include "stb_image.h"
+
+using namespace jcalgo::kmeans;
+
+void kmeansCallback(
+    size_t currentEpoch, size_t totalEpoch, double globalCenterDelta,
+    const KMeansResult& result, bool& stop, void* userdata)
+{
+    (void) result;
+    (void) stop;
+    (void) userdata;
+
+    printf("轮数 [%zu/%zu]  全局中心偏移量 %lf\n", currentEpoch, totalEpoch, globalCenterDelta);
+}
+
+DoubleVectorList ucharToDoubleVectorList(
+    const unsigned char* data, int width, int height, int channels)
+{
+    const size_t pixels = (size_t) width * (size_t) height;
+    DoubleVectorList result;
+    result.reserve(pixels);
+
+    for (size_t i = 0; i < pixels; ++i)
+    {
+        std::vector<double> v(channels);
+        const size_t base = i * (size_t) channels;
+
+        for (int c = 0; c < channels; ++c)
+            v[c] = (double) data[base + (size_t) c];
+
+        result.emplace_back(std::move(v));
+    }
+
+    return result;
+}
+
+int main(int argc, char* argv[])
+{
+    std::string filepath;
+    printf("输入图像文件路径：\n");
+    std::cin >> filepath;
+
+    // 读取图像数据
+    DoubleVectorList datas;
+    {
+        int width = 0, height = 0, channels = 0;
+        stbi_uc* d = stbi_load(filepath.c_str(), &width, &height, &channels, 3);
+        if (!d)
+        {
+            printf("Failed to load the image: %s\n", filepath.c_str());
+            return 1;
+        }
+        else
+        {
+            printf(
+                "Successfully load the image: %s, image size: [%d, %d]\n",
+                filepath.c_str(), width, height);
+        }
+
+        datas = ucharToDoubleVectorList(d, width, height, channels);
+        stbi_image_free(d);
+    }
+
+    constexpr size_t     k = 5;
+    constexpr size_t     totalEpoch = 100;
+    constexpr double     esp = 0.01;
+    constexpr KMeansFlag flag = KMEANS_RANDOM_CENTER;
+    auto result = kmeans(datas, k, flag, totalEpoch, esp, &kmeansCallback, nullptr);
+
+    auto clusters = createClusters(datas, result);
+
+    // 输出图像主题色
+    for (const auto& cluster : clusters)
+    {
+        const auto& center = cluster.center;
+        printf(
+            "RGB: [%d, %d, %d] Num: %zu\n",
+            (int) center[0], (int) center[1], (int) center[2], cluster.datas.size());
+    }
+
+    return 0;
+}
